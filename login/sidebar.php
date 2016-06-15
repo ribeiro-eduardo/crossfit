c8de0315b3e4ec6a35dee93763bf63401f37d2d7
eduardo95 <edu0995@gmail.com>
2016-06-15 00:20:43 -0300

    remocao pasta login

--- login/sidebar.php
+++
@@ -1,9 +0,0 @@
-				
-                <div id="sidebar">
-                	<ul class="sideNav">
-                        <li><a href="exemplo.php">Página exemplo <span>&raquo;</span></a></li>
-                        <li><a href="usuario.php">Usuário <span>&raquo;</span></a></li>
-                    </ul>
-                    <!-- // .sideNav -->
-                </div>    
-                <!-- // #sidebar -->
--- login/teste.php
+++
@@ -1,6 +0,0 @@
-
-
-<form action ="teste2.php" enctype="multipart/form-data" method="POST">
-<input type="file" name="pdf" id="pdf">
-<input type="submit" name="envia" value="enviar">
-</form> 
