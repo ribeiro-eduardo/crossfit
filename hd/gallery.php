00cfb8f7ca1528ddfc6f6c46c3ce3fae09f77053
andressalitwinski <andressalitwinski@gmail.com>
2016-07-29 16:10:12 -0300

    ajustado tamanho das imagens gallery e posicionamento do botão

--- hd/gallery.php
+++ hd/gallery.php
@@ -50,12 +50,12 @@ $galerias = $galeriasBO->get($galeriasVO);
                             $fotos = $galeriaFotosBO->get($galeriaFotosVO);
                             ?>
                             <div class="col-sm-4 col-xs-12">
-                                <figure class="wow fadeInLeft animated portfolio-item animated" data-wow-duration="500ms" data-wow-delay="0ms" style="visibility: visible; animation-duration: 300ms; -webkit-animation-duration: 300ms; animation-delay: 0ms; -webkit-animation-delay: 0ms; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
-                                    <div class="img-wrapper" style="height: 240px;">
-                                        <img src="../galerias/<?=$id?>/<?=$fotos[0]['nome']?>" class="img-responsive" alt="this is a title">
+                                <figure class="wow fadeInLeft animated portfolio-item animated" data-wow-duration="500ms" data-wow-delay="0ms" style="visibility: visible; animation-duration: 300ms; -webkit-animation-duration: 300ms; animation-delay: 0ms; -webkit-animation-delay: 0ms; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft; background: none;">
+                                    <div class="img-wrapper" style="height: 240px; background: url('../galerias/<?=$id?>/<?=$fotos[0]['nome']?>') no-repeat 50%; background-size: cover;">
+                                        <!-- <img src="../galerias/<?=$id?>/<?=$fotos[0]['nome']?>" class="img-responsive" alt="this is a title"> -->
                                         <div class="overlay">
-                                            <div class="buttons">
-                                                <a rel="group<?=$i?>" class="fancybox" href="../galerias/<?=$id?>/<?=$fotos[0]['nome']?>"><?=$galerias[$i]['nome']?></a>
+                                            <div>
+                                                <a rel="group<?=$i?>" class="fancybox" href="../galerias/<?=$id?>/<?=$fotos[0]['nome']?>" style="margin-top: 101px;"><?=$galerias[$i]['nome']?></a>
                                             </div>
                                             <? for($j = 1; $j < count($fotos); $j++){ ?>
                                             <a class="fancybox hide" rel="group<?=$i?>" href="../galerias/<?=$id?>/<?=$fotos[$j]['nome']?>"></a>
