a8e7120d6b0580d11b4ddca85a1c3dc2617479da
eduardo95 <edu0995@gmail.com>
2016-08-04 17:46:17 -0300

    login no hd

--- hd/login.php
+++ hd/login.php
@@ -1,5 +1,21 @@
 <?php
 include("header.php");
+
+$erro = $_GET["e"];
+$display = "display: none";
+
+if($erro == 1){
+    //não é admin
+    $erro = "Ops! Esse usuário não é um administrador!";
+    $display = "";
+}elseif($erro == 2){
+    //usuário inexistente
+    $erro = "Ops! Usuário e/ou senha incorretos!";
+    $display = "";
+}elseif($erro == 3){
+    //logout
+    $erro = "";
+}
 ?>
 
 <!--
@@ -32,21 +48,22 @@ Global Page Section Start
 <section>
     <div class="container">
         <div class="row">
+            <span><?=$erro?></span>
           <div class="wow fadeInRight" data-wow-delay=".3s" >
-            <form class="login text-center" method="post">
+            <form class="login text-center" method="post" action="processausuarios.php">
               <div class="form-group input-group">
                 <span class="input-group-addon">
                   <span class="ion-person"></span>
                 </span>
-                <input class="form-control" placeholder="Usuário">
+                <input class="form-control" placeholder="Usuário" name="login">
               </div>
               <div class="form-group input-group">
                 <span class="input-group-addon">
                   <span class="ion-unlocked"></span>
                 </span>
-                <input type="password" class="form-control" placeholder="Senha">
+                <input type="password" class="form-control" placeholder="Senha" name="senha">
               </div>
-              <button type="submit" class="btn btn-details">Entrar</button>
+              <button type="submit" name="entrar" value="entrar" class="btn btn-details">Entrar</button>
             </form>
           </div>
         </div>
@@ -63,7 +80,7 @@ Global Page Section Start
         <div class="row">
             <div class="col-md-12">
                 <div class="block">
-                    <h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">Ainda não é nosso aluno?</h1>
+                    <h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">Ainda não é nosso aluno?</h2>
                     <p class="wow fadeInDown" data-wow-delay=".5s" data-wow-duration="500ms">Agende uma aula experimental. Esperamos por você!</p>
                     <a href="contact.php" class="btn btn-default btn-contact wow fadeInDown" data-wow-delay=".7s" data-wow-duration="500ms">Entre em contato</a>
                 </div>
