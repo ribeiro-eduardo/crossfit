7485c2783d23abb13fa82e01336828b3bb57364e
andressalitwinski <andressalitwinski@gmail.com>
2016-08-12 14:47:15 -0300

    página de atletas

--- hd/atletas.php
+++ hd/atletas.php
@@ -28,6 +28,65 @@ Global Page Section Start
     </div>
     </section><!--/#Page header-->
 
+    <div class="container" style="margin-top: 60px; margin-bottom: 60px">
+
+      <div class="busca text-center" style="font-size: 16px;">
+        <input placeholder="Faça sua busca aqui..." />
+        <span class="ion-search"></span>
+      </div>
+
+      <div id="atletas" style="padding-top: 100px; color: #fcfcfc;">
+
+        <!-- div inserida para cada atleta -->
+          <div class="col-md-4" style="margin-bottom: 40px;">
+            <a href="#">
+                <img class="img-circle col-md-2 img-busca" src="images/coach2.jpg">
+                <div>
+                  <h3>Tio Patinhas<span><img src="images/coach.png"></span></h3>
+                  <p>29 anos</p>
+                </div>
+            </a>
+          </div>
+        <!-- fim da div -->
+
+          <div class="col-md-4" style="margin-bottom: 40px;">
+            <a href="#">
+              <img class="img-circle col-md-2 img-busca" src="images/coach2.jpg">
+              <div>
+                <h3>Tio Patinhas<span><img src="images/athlete.png"></span></h3>
+                <p>29 anos</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4" style="margin-bottom: 40px;">
+            <a href="#">
+              <img class="img-circle col-md-2 img-busca" src="images/coach2.jpg">
+              <div>
+                <h3>Tio Patinhas<span><img src="images/coach.png"></span></h3>
+                <p>29 anos</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4" style="margin-bottom: 40px;">
+            <a href="#">
+              <img class="img-circle col-md-2 img-busca" src="images/coach2.jpg">
+              <div>
+                <h3>Tio Patinhas<span><img src="images/coach.png"></span></h3>
+                <p>29 anos</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4" style="margin-bottom: 40px;">
+            <a href="#">
+              <img class="img-circle col-md-2 img-busca" src="images/coach2.jpg">
+              <div>
+                <h3>Tio Patinhas<span><img src="images/coach.png"></span></h3>
+                <p>29 anos</p>
+              </div>
+            </a>
+          </div>
+      </div>
+  </div>
 
 
 
