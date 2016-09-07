1e9b521e79e34f77e8b8d19505e3ef7a2f2027fe
eduardo95 <edu0995@gmail.com>
2016-09-07 03:31:01 -0300

    ajuste

--- hd/ranking.php
+++ hd/ranking.php
@@ -108,41 +108,7 @@ Global Page Section Start
     </div>
     <div class="row" style="margin-top: 100px">
         <div class="col-xs-12 text-center">
-
-            <!--            <div class="text-right">-->
-            <!--                <button type="button" class="btn btn-default btn-lg"-->
-            <!--                        style="background: none; border: none; color: #ec001c">-->
-            <!--                    <span class="ion-arrow-up-b" aria-hidden="true"></span>-->
-            <!--                </button>-->
-            <!---->
-            <!--                <button type="button" class="btn btn-default btn-lg"-->
-            <!--                        style="background: none; border: none; color: #ec001c">-->
-            <!--                    <span class="ion-arrow-down-b" aria-hidden="true"></span>-->
-            <!--                </button>-->
-            <!--            </div>-->
-
-
-            <table class="table table-hover" id="listagem">
-                <!--linha para cada atleta do ranking-->
-                <!--              <tr>-->
-                <!--                <td>-->
-                <!--                  <a href="#" style="color: #5f5f5f;">-->
-                <!--                    <div class="col-sm-1">-->
-                <!--                      <h1 style="color: #e5001c">1</h1>-->
-                <!--                    </div>-->
-                <!--                      <div class="col-md-offset-2 col-md-6">-->
-                <!--                        <div class="circle-avatar col-md-4"
-                                                style="background: url('fotos-coaches/img-retangular.png') no-repeat; width: 150px; height: 150px;"></div>-->
-                <!--                        <h3><span><img src="images/athlete.png"></span>Meu nome horroroso</h3>-->
-                <!--                        <p>21 anos</p>-->
-                <!--                        <p style="font-weight: bold; color: #e5001c">meu tempinho</p>-->
-                <!--                      </div>-->
-                <!--                  </a>-->
-                <!--                </td>-->
-                <!--              </tr>-->
-                <!--fim da linha para cada atleta do ranking-->
-            </table>
-
+            <table class="table table-hover" id="listagem"></table>
         </div>
     </div>
 </div>
